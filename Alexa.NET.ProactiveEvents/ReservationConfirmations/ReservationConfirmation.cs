75f2eeee79b560962fb72e74ceaec718bbdab534
Steven Pears <steven.pears@live.co.uk>
2018-12-26 20:24:54 +0000

    add localization support for reservations

--- Alexa.NET.ProactiveEvents/ReservationConfirmations/ReservationConfirmation.cs
+++ Alexa.NET.ProactiveEvents/ReservationConfirmations/ReservationConfirmation.cs
@@ -13,5 +13,18 @@ namespace Alexa.NET.ProactiveEvents.ReservationConfirmations
         {
             Payload = new ReservationConfirmationPayload(status,occasion);
         }
+
+        public override IEnumerable<KeyValuePair<string, List<LocaleAttribute>>> GetLocales()
+        {
+            var keyvalues = new Dictionary<string, List<LocaleAttribute>>();
+            if (Payload.Occasion.Broker != null)
+            {
+                keyvalues.Add("brokerName",Payload.Occasion.Broker.Name);
+            }
+            keyvalues.Add("subject",Payload.Occasion.Subject);
+            keyvalues.Add("providerName", Payload.Occasion.Provider.Name);
+
+            return keyvalues;
+        }
     }
 }
